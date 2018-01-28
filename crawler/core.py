23e8c77089f2537e5d71b34b44cbd4b97bb5b0b3
DESKTOP-U3SLR66\ScorpionIPX <scorpionipx@gmail.com>
2018-01-28 13:53:19 +0200

    Created main directory tree

---
+++ crawler/core.py
@@ -0,0 +1,13 @@
+class Crawler:
+    """Crawler
+        Class used to handle remote controlled device.
+    """
+    def __init__(self):
+        """Constructor
+        """
+        pass
+
+
+
+
+
---
+++ crawler/utils/connection/host.py
@@ -0,0 +1,8 @@
+class Host:
+    """Host
+        Class used to hande internet connection as host(slave).
+    """
+    def __init__(self):
+        """Constructor
+        """
+        pass
